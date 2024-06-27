fb0cdc22afae48d5646f2d9b4261f814c9a29773
felipe thaleson <thaleson19@hotmail.com>
2024-06-27 14:02:52 -0300

    Update __init__.py

--- comunidade/__init__.py
+++ comunidade/__init__.py
@@ -3,13 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+import os 
 app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = '53c489b6e5a330e302256da286791c88'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 database = SQLAlchemy(app)
 migrate = Migrate(app, database)
